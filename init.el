16c8f259137a3e5d89eb6b6c7cc945b13d46a6bd
Greg Hendershott <git@greghendershott.com>
2019-06-01 22:27:50 -0400

    Racket 7.3

--- init.el
+++ init.el
@@ -772,7 +772,7 @@
          ;; under /mnt/c, we also need this:
          (setq racket-adjust-run-rkt #'racket-wsl-to-windows))
         (linux-p
-         (setq racket-program "~/racket-7.2/bin/racket")
+         (setq racket-program "~/racket/7.3/bin/racket")
          (setq racket-path-from-emacs-to-racket-function #'identity)
          (setq racket-path-from-racket-to-emacs-function #'identity)
          (setq racket-adjust-run-rkt #'identity))
